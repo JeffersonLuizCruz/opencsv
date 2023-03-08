8638c265aff54ac1cb9abf1344d29e85ec671d6f
JeffersonLuizCruz <jefferson.luiz.cruz@gmail.com>
2023-03-07 21:59:30 -0300

    feat:init

---
+++ src/main/java/com/opencsv/ProjectOpencsvApplication.java
@@ -0,0 +1,13 @@
+package com.opencsv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectOpencsvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectOpencsvApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/opencsv/ProjectOpencsvApplicationTests.java
@@ -0,0 +1,13 @@
+package com.opencsv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectOpencsvApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
